80b924c3a0866aece0e4240d9b169630394c97bb
Alexey Lapshev <alapshev@emphasoft.com>
2021-08-27 23:31:23 +0300

    added js enabled

--- popup.js
+++ popup.js
@@ -1,6 +1,10 @@
-let inputField = getInput()
+let inputField = getInput(),
+  toggle = document.getElementById("toggle"),
+  jsEnabled = false
 
-function getInput () {
+changeTitle()
+
+function getInput() {
   return document.getElementById("paste-here");
 }
 
@@ -9,6 +13,40 @@ function openHtml(html) {
   chrome.tabs.create({url: url});
 }
 
-inputField.addEventListener("paste", async (event) => {
-  setTimeout(() => openHtml(getInput().value), 0.001);
+inputField.addEventListener("paste", async () => {
+  setTimeout(() => openHtml(prepareHtml(getInput().value)), 0.001);
 });
+
+toggle.addEventListener("click", async () => {
+  jsEnabled = !jsEnabled
+  changeTitle()
+});
+
+function prepareHtml(html) {
+  let doc = new DOMParser().parseFromString(html, "text/html")
+
+  if (!jsEnabled) {
+    let scripts = doc.querySelectorAll("script"),
+      searchString = "text/javascript",
+      replaceString = "application/json"
+    scripts.forEach(script => {
+      let scriptHtml = script.outerHTML
+      if (scriptHtml.includes(searchString)) {
+        scriptHtml = scriptHtml.replace(searchString, replaceString)
+      } else {
+        scriptHtml = scriptHtml.replace("<script", `<script type="${replaceString}"`)
+      }
+      script.outerHTML = scriptHtml
+    })
+  }
+  return new XMLSerializer().serializeToString(doc)
+}
+
+function changeTitle() {
+  let toggleTitle = document.getElementById("toggle-name")
+  if (jsEnabled) {
+    toggleTitle.textContent = "JS is enabled"
+  } else {
+    toggleTitle.textContent = "JS is disabled"
+  }
+}
